e0b6ff51084abf09d4c1370372bbf21293746265
Ro-hitMali <rm4701740@gmail.com>
2023-01-29 13:08:03 +0530

    initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
+
+function Login() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Rohit Mali", age: 25, email: "rm4701740@gmail.com" }));
+        }}
+      >
+        Login
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        LOGOUT
+      </button>
+    </div>
+  );
+}
+
+export default Login;
